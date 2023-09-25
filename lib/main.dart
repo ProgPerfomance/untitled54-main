677615bea02bffc8bcd373f9523b3fed1bb7e236
evaranshark <evarancorp@gmail.com>
2023-09-25 16:52:53 +0300

    Init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/feature/artists_name/artists_name.dart';
 import 'package:myapp/feature/home/home.dart';
@@ -8,8 +9,13 @@ import 'package:myapp/feature/registration/customer/view/registration_for%20cust
 import 'package:myapp/feature/registration/delivery/view/registration_for_delivery.dart';
 import 'package:myapp/feature/registration/register/view/registration_screen.dart';
 import 'package:myapp/feature/setting/setting.dart';
+import 'package:myapp/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
