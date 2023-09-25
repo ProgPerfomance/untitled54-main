1e19d2d8811e92d2e83c8f72cecebdbcfec93ce0
doge <jekcatpopov@gmail.com>
2023-09-25 18:12:32 +0300

    config files

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,4 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthController {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  void signUpUser() {
+    _auth.signInWithEmailAndPassword(email: , password: password)
+    _firestore.collection('buyers').doc('').set({
+
+    });
+  }
 }
